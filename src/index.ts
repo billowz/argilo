4038bc66006a31b48412444412388916f2a2fa7b
Billow Z <billowz@hotmail.com>
2019-04-18 19:31:02 +0800

    fix: main

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+/**
+ *
+ * @author Tao Zeng <tao.zeng.zt@qq.com>
+ * @module main
+ * @preferred
+ * @created Wed Nov 21 2018 10:21:20 GMT+0800 (China Standard Time)
+ * @modified Mon Apr 08 2019 14:07:50 GMT+0800 (China Standard Time)
+ */
+export * from './utils'
+export * from './assert'
+export * from './format'
+export * from './path'
+export * from './list'
+export * from './nextTick'
+export * from './ast'
+export * from './observer'
